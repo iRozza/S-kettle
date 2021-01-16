554b664f7fcd98a3e607544ac1e29a232b84f3aa
iRozza <abdelrahman.reda.1996@gmail.com>
2021-01-17 00:41:52 +0200

    Initial commit with the home page except the animation and the scroll down

---
+++ js/plugins.js
@@ -0,0 +1,26 @@
+$("document").ready(function () {
+  $(window).on("load", function () {
+    $(".loading-screen .spinner").fadeOut(1000, function () {
+      $(this)
+        .parent()
+        .fadeOut(1000, function () {
+          $("body").css("overflow", "auto")
+          $(this).remove()
+        })
+    })
+  })
+
+  /* scroll top */
+  scrollButton = $("#scroll-top")
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 700) {
+      scrollButton.show() // show the button
+    } else {
+      scrollButton.hide() // hide the button
+    }
+
+    document.getElementById("scroll-top").onclick = function () {
+      $("html,body").animate({ scrollTop: 0 }, 200)
+    }
+  })
+})
